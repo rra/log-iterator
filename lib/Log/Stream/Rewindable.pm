162d881b4763ba74635098e037f2f8b4fc774c0c
Russ Allbery <rra@stanford.edu>
2013-01-23 13:32:20 -0800

    Add Log::Stream::Rewindable
    This is a wrapper around a stream that allows it to be rewinded or
    elements prepended to it.

---
+++ lib/Log/Stream/Rewindable.pm
@@ -0,0 +1,360 @@
+# Log::Stream::Rewindable -- Make an infinite log stream partly rewindable.
+#
+# Written by Russ Allbery <rra@stanford.edu>
+# Copyright 2013
+#     The Board of Trustees of the Leland Stanford Junior University
+
+##############################################################################
+# Modules and declarations
+##############################################################################
+
+package Log::Stream::Rewindable;
+
+use 5.010;
+use autodie;
+use strict;
+use warnings;
+
+use base qw(Log::Stream);
+
+use Carp qw(croak);
+
+# Module version.  Waiting for Perl 5.12 to switch to the new package syntax.
+our $VERSION = '1.00';
+
+##############################################################################
+# Implementation
+##############################################################################
+
+# Create a new Log::Stream::Rewindable object that supports bookmarking,
+# rewinding, and prepending.  This differs from a normal stream by adding two
+# pieces of state: the saved elements, which are the elements between the
+# current bookmark and the stream head, and the queued elements, which are
+# elements returned to the stream by either prepend() or unwind().
+#
+# $class  - Class of the object being created
+# $stream - The underlying stream to use as a data source
+#
+# Returns: New Log::Stream::Rewindable object
+sub new {
+    my ($class, $stream) = @_;
+
+    # Our tail function reads from the queue by preference.  Bookmarking has
+    # to be done in get(), since we don't want to save the head element.
+    my $self = {};
+    my $tail = sub {
+        if ($self->{queue}) {
+            my $element = pop @{ $self->{queue} };
+            if (!@{ $self->{queue} }) {
+                $self->{queue} = undef;
+            }
+            return $element;
+        } else {
+            return $stream->get;
+        }
+    };
+
+    # Build and return the object.
+    $self->{head} = undef;
+    $self->{tail} = $tail;
+    bless $self, $class;
+    return $self;
+}
+
+# Set a bookmark.  Do this by creating an anonymous array in $self->{saved}.
+# If there is already a bookmark, call discard() first.
+#
+# $self - The Log::Stream::Rewindable object
+#
+# Returns: True
+sub bookmark {
+    my ($self) = @_;
+    if ($self->{saved}) {
+        $self->discard;
+    }
+    $self->{saved} = [];
+    return 1;
+}
+
+# Discard the bookmark and any saved elements.
+#
+# $self - The Log::Stream::Rewindable object
+#
+# Returns: True
+#  Throws: Text exception if no bookmark is set
+sub discard {
+    my ($self) = @_;
+    if (!$self->{saved}) {
+        croak('No bookmark set in stream');
+    }
+    delete $self->{saved};
+    return 1;
+}
+
+# Override get() to store retrieved objects in the saved array if we have a
+# bookmark set.  We have a bookmark iff $self->{saved} is present.
+#
+# $self - The Log::Stream::Rewindable object
+#
+# Returns: Current value of head
+sub get {
+    my ($self) = @_;
+    my $element = $self->SUPER::get;
+    if ($self->{saved} && defined $element) {
+        push @{ $self->{saved} }, $element;
+    }
+    return $element;
+}
+
+# Prepend the provided elements to the head of the stream by adding them to
+# the queue and making the current head the first element.
+#
+# $self     - The Log::Stream::Rewindable object
+# @elements - The elements to add to the stream
+#
+# Returns: True
+sub prepend {
+    my ($self, @elements) = @_;
+
+    # If we have a defined head, that means we've pulled an element off the
+    # stream into it, and we need to save that element.  Do so by adding it
+    # to the elements we're prepending and then clearing head so that we'll
+    # retrieve a new element on the next head() or get().
+    if (defined $self->{head}) {
+        push @elements, $self->{head};
+        $self->{head} = undef;
+    }
+
+    # Put the elements into the queue.
+    $self->{queue} ||= [];
+    push @{ $self->{queue} }, reverse @elements;
+
+    # If the stream was empty, we destroyed the tail.  Rebuild it.
+    if (!defined $self->{tail}) {
+        $self->{tail} = sub { return pop @{ $self->{queue} } };
+    }
+    return 1;
+}
+
+# Rewind the stream to the bookmark and then drop the bookmark.  We do this by
+# adding the saved elements to the stream using prepend and then discarding
+# the bookmark.
+#
+# $self - The Log::Stream::Rewindable object
+#
+# Returns: True
+#  Throws: Text exception if no bookmark is set
+sub rewind {
+    my ($self) = @_;
+    if (!$self->{saved}) {
+        croak('No bookmark set in stream');
+    }
+    $self->prepend($self->saved);
+    $self->discard;
+    return 1;
+}
+
+# Return a list of all elements between the bookmark and the current position
+# in the stream.
+#
+# $self - The Log::Stream::Rewindable object
+#
+# Returns: True
+#  Throws: Text exception if no bookmark is set
+sub saved {
+    my ($self) = @_;
+    if (!$self->{saved}) {
+        croak('No bookmark set in stream');
+    }
+    return @{ $self->{saved} };
+}
+
+##############################################################################
+# Module return value and documentation
+##############################################################################
+
+1;
+__END__
+
+=for stopwords
+API Kaufmann MERCHANTABILITY NONINFRINGEMENT seekable sublicense
+
+=head1 NAME
+
+Log::Stream::Rewindable - Make an infinite log stream partly rewindable
+
+=head1 SYNOPSIS
+
+    use Log::Stream::File;
+    use Log::Stream::Rewindable;
+    my $path   = '/path/to/some/log';
+    my $stream = Log::Stream::File->new({ file => $path });
+
+    # Wrap the stream to make it rewindable.
+    $stream = Log::Stream::Rewindable->new($stream);
+
+    # Set a bookmark and consume some elements.
+    $stream->bookmark;
+    $stream->get;
+    $stream->get;
+
+    # Returns the elements retrieved with get() since the bookmark.
+    my @elements = $stream->saved;
+
+    # Rewinds the stream to the bookmark.
+    $stream->rewind;
+
+    # Adds elements to the front of the stream.
+    $stream->prepend(qw(one two three));
+
+    # Discard removes the bookmark and any saved elements.
+    $stream->bookmark;
+    my $element = $stream->get;
+    $stream->discard;
+
+=head1 REQUIREMENTS
+
+Perl 5.10 or later.
+
+=head1 DESCRIPTION
+
+Log::Stream::Rewindable wraps an arbitrary stream (or, for that matter,
+any other object that supports the get() method) and makes it partly
+rewindable.  The stream does not become fully seekable, but the caller can
+set a (single) bookmark, rewind the stream to the bookmark, or retrieve
+all stream elements since the bookmark.
+
+Be aware that all elements between the bookmark and the current head of
+the stream are cached in memory, so setting a bookmark and then reading
+lots of elements from a stream without calling discard() or rewind() can
+consume a significant amount of memory.
+
+=head1 CLASS METHODS
+
+=over 4
+
+=item new(STREAM)
+
+Create a new rewindable stream that wraps STREAM.  STREAM is treated as a
+duck-typed stream, which means that it can be any object that supports the
+get() method with the expected stream semantics.
+
+=back
+
+=head1 INSTANCE METHODS
+
+=over 4
+
+=item bookmark()
+
+Sets a bookmark at the current stream position.  The stream can then be
+rewound to this point with rewind(), and all of the elements between the
+bookmark and the current position can be retrieved with saved().  A
+bookmark must be set before rewind(), saved(), or discard() may be called.
+
+If a bookmark is already set and bookmark() is called again, it is
+equivalent to calling discard() followed by bookmark().  All saved stream
+elements are discarded, and the bookmark is reset to the current stream
+position.
+
+=item discard()
+
+Discards the current bookmark and any saved data.  After discard(), the
+stream can no longer be rewound until bookmark() is called again.
+
+=item get()
+
+Returns the next element in the stream and consumes it.  Repeated calls to
+get() will read through the entire stream, returning each record once.
+Returns undef at the end of the stream.
+
+=item head()
+
+Returns the next element in the stream without consuming it.  Repeated
+calls to head() without an intervening call to get() will keep returning
+the same record.  Returns undef at the end of the stream.
+
+=item prepend(ELEMENT[, ELEMENT ...])
+
+Prepend the provided elements to the stream.  The first element given will
+become the new head element, and those elements will be returned in
+sequence before any subsequent items in the stream.  This can be used
+along with saved() to remove some elements from a stream and then return
+the rest for further processing.
+
+prepend() will make a shallow copy of the provided elements, but not a
+deep copy.
+
+If rewind() is used after prepend(), the prepended elements will remain in
+the stream, but will be after the elements re-added to the stream by
+rewind().  In other words, prepend() behaves as if its arguments were
+inserted into the stream before the current head, even during rewind().
+
+=item rewind()
+
+Rewind the stream to the current bookmark and drop the bookmark.  The
+elements of the stream retrieved since the last call to bookmark() can now
+be retrieved again exactly as before.  bookmark() must be called before
+rewind() can be called.
+
+=item saved()
+
+Returns, as a list, all of the elements between the bookmark and the
+current position in the stream.  bookmark() must be called before calling
+saved().  The saved elements are not affected by this call; to also remove
+them from the stream, call discard() after saved().
+
+This can be used along with prepend() to remove some elements from a
+stream and then return the rest for further processing.
+
+=back
+
+=head1 DIAGNOSTICS
+
+=over 4
+
+=item No bookmark set in stream
+
+(F) discard(), rewind(), or saved() was called while there was no bookmark
+set in the stream.
+
+=back
+
+=head1 AUTHOR
+
+Russ Allbery <rra@stanford.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 The Board of Trustees of the Leland Stanford Junior
+University
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+=head1 SEE ALSO
+
+L<Log::Stream>
+
+Dominus, Mark Jason.  I<Higher Order Perl>.  San Francisco: Morgan
+Kaufmann Publishers, 2005.  Print.
+
+The current version of this module is always available from its web site
+at L<http://www.eyrie.org/~eagle/software/log-stream/>.
+
+=cut
---
+++ t/stream/rewindable.t
@@ -0,0 +1,153 @@
+#!/usr/bin/perl
+#
+# Test for rewindable streams.
+#
+# Written by Russ Allbery <rra@stanford.edu>
+# Copyright 2013
+#     The Board of Trustees of the Leland Stanford Junior University
+#
+# See LICENSE for licensing terms.
+
+use 5.010;
+use autodie;
+use strict;
+use warnings;
+
+use Test::More tests => 70;
+
+# Load the modules.
+BEGIN {
+    use_ok('Log::Stream');
+    use_ok('Log::Stream::Rewindable');
+}
+
+# Build a trivial little stream.
+my @data   = qw(first second third fourth);
+my $code   = sub { return shift @data };
+my $stream = Log::Stream->new({ code => $code });
+isa_ok($stream, 'Log::Stream');
+
+# Make it rewindable.
+$stream = eval { Log::Stream::Rewindable->new($stream) };
+is($@, q{}, 'No exceptions on filter object creation');
+if ($stream) {
+    isa_ok($stream, 'Log::Stream::Rewindable');
+} else {
+    ok(0, 'Object creation failed');
+    BAIL_OUT('cannot continue without Log::Stream::Rewindable object');
+}
+
+# Normal expected output if we use head and get on the first element.
+is($stream->head, 'first',  'First head() is first');
+is($stream->head, 'first',  '...and returns the same when called again');
+is($stream->get,  'first',  'First get() is first');
+is($stream->head, 'second', 'Head is now second');
+
+# Set a bookmark and then consume a couple of elements.
+ok($stream->bookmark, 'Bookmark successful');
+is($stream->get,  'second', 'Second get() is second');
+is($stream->get,  'third',  'Third get() is third');
+is($stream->head, 'fourth', 'Head is now fourth');
+
+# Rewind and we should be back where we were.
+ok($stream->rewind, 'Rewind successful');
+is($stream->head, 'second', 'Head is second after rewind');
+
+# Set another bookmark and consume the entire stream.
+ok($stream->bookmark, 'Bookmark successful');
+is($stream->get,  'second', 'Second get() is second');
+is($stream->get,  'third',  'Third get() is third');
+is($stream->get,  'fourth', 'Fourth get() is fourth');
+is($stream->head, undef,    'Head is now undef');
+is($stream->get,  undef,    'Get returns undef');
+
+# Test rewinding from exhausting the stream.
+ok($stream->rewind, 'Rewind successful');
+is($stream->get, 'second', 'Second get() is second after rewinding');
+
+# Test bookmarking in the middle of rewound elements.
+ok($stream->bookmark, 'Rewind successful');
+is($stream->get, 'third',  'Third get() is third');
+is($stream->get, 'fourth', 'Fourth get() is fourth');
+is($stream->get, undef,    'Get returns undef');
+ok($stream->rewind, 'Rewind successful');
+is($stream->get, 'third',  'Third get() is third after rewinding');
+is($stream->get, 'fourth', 'Fourth get() is fourth');
+is($stream->get, undef,    'Get returns undef');
+
+# Test prepending some data to the stream.
+ok($stream->prepend(qw(fifth sixth)), 'Prepend successful');
+is($stream->head, 'fifth', 'Head is now fifth');
+is($stream->get,  'fifth', 'Fifth get() is fifth');
+
+# Test bookmarking in the middle of prepended data.
+ok($stream->bookmark, 'Bookmark successful');
+is($stream->get, 'sixth', 'Sixth get() is sixth');
+is($stream->get, undef,   'Get returns undef');
+ok($stream->rewind, 'Rewind successful');
+is($stream->get, 'sixth', 'Get returns sixth after rewind');
+
+# Refresh the stream.
+@data = qw(first second third fourth);
+$stream = Log::Stream::Rewindable->new(Log::Stream->new({ code => $code }));
+isa_ok($stream, 'Log::Stream::Rewindable');
+
+# Test discarding bookmarked data.
+ok($stream->bookmark, 'Bookmark successful');
+is($stream->get, 'first',  'First get() is first after refresh');
+is($stream->get, 'second', 'Second get() is second');
+ok($stream->discard, 'Discard successful');
+my $return = eval { $stream->rewind };
+ok(!$return, 'Rewind fails after discard');
+like(
+    $@,
+    qr{ \A No [ ] bookmark [ ] set [ ] in [ ] stream [ ] at [ ] }xms,
+    '...with correct error'
+);
+$return = eval { $stream->discard };
+ok(!$return, 'Discard fails after discard');
+like(
+    $@,
+    qr{ \A No [ ] bookmark [ ] set [ ] in [ ] stream [ ] at [ ] }xms,
+    '...with correct error'
+);
+
+# Test retreiving the bookmarked data.
+ok($stream->bookmark, 'Bookmark successful');
+is($stream->get, 'third',  'Third get() is third');
+is($stream->get, 'fourth', 'Fourth get() is fourth');
+is($stream->get, undef,    'Found end of stream');
+is_deeply([$stream->saved], [qw(third fourth)], 'Saved data is correct');
+ok($stream->rewind, '...and can still rewind');
+is($stream->head, 'third', '...to the correct point');
+$return = eval { $stream->rewind };
+ok(!$return, '...but cannot rewind again');
+like(
+    $@,
+    qr{ \A No [ ] bookmark [ ] set [ ] in [ ] stream [ ] at [ ] }xms,
+    '...with correct error'
+);
+$return = eval { $stream->saved };
+ok(!$return, '...and cannot retrieve saved data');
+like(
+    $@,
+    qr{ \A No [ ] bookmark [ ] set [ ] in [ ] stream [ ] at [ ] }xms,
+    '...with correct error'
+);
+ok($stream->bookmark, 'Bookmark successful');
+is_deeply([$stream->saved], [], '...and immediate saved returns empty list');
+
+# Refresh the stream.
+@data = qw(first second third fourth);
+$stream = Log::Stream::Rewindable->new(Log::Stream->new({ code => $code }));
+isa_ok($stream, 'Log::Stream::Rewindable');
+
+# Calling bookmark twice does an implicit discard.
+ok($stream->bookmark, 'Bookmark successful');
+is($stream->get, 'first',  'First get() is first after refresh');
+is($stream->get, 'second', 'Second get() is second');
+ok($stream->bookmark, 'Calling bookmark again is successful');
+is_deeply([$stream->saved], [], '...and there are no saved elements');
+is($stream->get, 'third', 'Third get() is third');
+ok($stream->rewind, 'Rewind successful');
+is($stream->head, 'third', '...and returns to the second bookmark');
