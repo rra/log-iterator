ebbbde49440f02fae7476d65f2f7dec2a89f282a
Russ Allbery <rra@stanford.edu>
2013-01-22 22:08:07 -0800

    New Log::Stream::Filter to filter a stream
    Takes a code reference and only returns those stream elements for
    which the code reference returns true.

---
+++ lib/Log/Stream/Filter.pm
@@ -0,0 +1,176 @@
+# Log::Stream::Filter -- Filter an infinite log stream
+#
+# Written by Russ Allbery <rra@stanford.edu>
+# Copyright 2013
+#     The Board of Trustees of the Leland Stanford Junior University
+
+##############################################################################
+# Modules and declarations
+##############################################################################
+
+package Log::Stream::Filter;
+
+use 5.010;
+use autodie;
+use strict;
+use warnings;
+
+use base qw(Log::Stream);
+
+# Module version.  Waiting for Perl 5.12 to switch to the new package syntax.
+our $VERSION = '1.00';
+
+##############################################################################
+# Implementation
+##############################################################################
+
+# Create a new Log::Stream::Filter object that applies the provided filter to
+# each element returned from the underlying stream.
+#
+# $class  - Class of the object being created
+# $filter - Code reference to the filter to apply
+# $stream - The underlying stream to use as a data source
+#
+# Returns: New Log::Stream::Filter object
+sub new {
+    my ($class, $filter, $stream) = @_;
+
+    # Wrap the provided filter in a sub that loops until a valid stream value
+    # is found.
+    my $code = sub {
+        my $head;
+      ELEMENT: {
+            $head = $stream->get;
+            return if !defined $head;
+            redo ELEMENT if !$filter->($head);
+        }
+        return $head;
+    };
+
+    # Build and return the object.
+    return $class->SUPER::new({ code => $code });
+}
+
+##############################################################################
+# Module return value and documentation
+##############################################################################
+
+1;
+__END__
+
+=for stopwords
+API Kaufmann MERCHANTABILITY NONINFRINGEMENT sublicense
+
+=head1 NAME
+
+Log::Stream::Filter - Filter an infinite log stream
+
+=head1 SYNOPSIS
+
+    use Log::Stream::Filter;
+    my $stream; # some existing stream
+
+    # Filter out lines of 40 characters or less.
+    my $code = sub {
+        my ($line) = @_;
+        return length($line) > 40;
+    };
+    $stream = Log::Stream::Filter->new($code, $stream);
+
+    # Read the next filtered line without consuming it.
+    my $record = $stream->head;
+
+    # The same, but consume it.
+    $record = $stream->get;
+
+=head1 REQUIREMENTS
+
+Perl 5.10 or later.
+
+=head1 DESCRIPTION
+
+Log::Stream::Filter is used to wrap an arbitrary code filter around a
+Log::Stream object (or, for that matter, any other object that supports
+the get() method).  It runs an arbitrary user-provided filter on each
+element returned by the underlying stream.  If the filter returns true,
+the element will be passed along; if it returns false, the next element
+will be read from the stream, continuing until the filter returns true or
+the end of the stream is reached.  Note that Log::Stream::Filter objects
+can be used interchangeably with Log::Stream objects and support the same
+API, so one can stack multiple transforms and filters on top of each
+other.
+
+The filter operation is based loosely on the infinite streams discussed in
+I<Higher Order Perl> by Mark Jason Dominus, but is not based on the code
+from that book and uses an object-oriented version of the interface.
+
+=head1 CLASS METHODS
+
+=over 4
+
+=item new(CODE, STREAM)
+
+Create a new filtered stream.  CODE will be called with each element of
+STREAM as its argument, and only those elements for which CODE returns
+true will be passed on.  STREAM is treated as a duck-typed stream, which
+means that it can be any object that supports the get() method with the
+expected stream semantics.
+
+=back
+
+=head1 INSTANCE METHODS
+
+=over 4
+
+=item head()
+
+Returns the next valid element in the stream without consuming it.
+Repeated calls to head() without an intervening call to get() will keep
+returning the same record.  Returns undef at the end of the stream.
+
+=item get()
+
+Returns the next valid element in the stream and consumes it.  Repeated
+calls to get() will read through the entire stream, returning each record
+once.  Returns undef at the end of the stream.
+
+=back
+
+=head1 AUTHOR
+
+Russ Allbery <rra@stanford.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 The Board of Trustees of the Leland Stanford Junior
+University
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+=head1 SEE ALSO
+
+L<Log::Stream>, L<Log::Stream::Transform>
+
+Dominus, Mark Jason.  I<Higher Order Perl>.  San Francisco: Morgan
+Kaufmann Publishers, 2005.  Print.
+
+The current version of this module is always available from its web site
+at L<http://www.eyrie.org/~eagle/software/log-stream/>.
+
+=cut
---
+++ t/stream/filter.t
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+#
+# Basic test for stream filters.
+#
+# Written by Russ Allbery <rra@stanford.edu>
+# Copyright 2013
+#     The Board of Trustees of the Leland Stanford Junior University
+#
+# See LICENSE for licensing terms.
+
+use 5.010;
+use autodie;
+use strict;
+use warnings;
+
+use File::Spec;
+
+use Test::More tests => 14;
+
+# Load the modules.
+BEGIN {
+    use_ok('Log::Stream');
+    use_ok('Log::Stream::Filter');
+}
+
+# Build a trivial little stream.
+my @data   = qw(first second third fourth);
+my $code   = sub { return shift @data };
+my $stream = Log::Stream->new({ code => $code });
+isa_ok($stream, 'Log::Stream');
+
+# Now, build a trivial filter.
+my $filter = sub { my ($element) = @_; return scalar $element =~ m{d}xms };
+$stream = eval { Log::Stream::Filter->new($filter, $stream) };
+is($@, q{}, 'No exceptions on filter object creation');
+if ($stream) {
+    isa_ok($stream, 'Log::Stream::Filter');
+} else {
+    ok(0, 'Object creation failed');
+    BAIL_OUT('cannot continue without Log::Stream::Filter object');
+}
+
+# Check the output.
+is($stream->head, 'second', 'First head() is second');
+is($stream->head, 'second', '...and returns the same when called again');
+is($stream->get,  'second', 'First get() is second');
+is($stream->head, 'third',  '...and now head() is second');
+is($stream->get,  'third',  'Second get() is third');
+is($stream->head, undef,    '...and now head() is undef');
+is($stream->get,  undef,    '...and get() is undef');
+is($stream->get,  undef,    '...and stays undef');
+is($stream->head, undef,    '...and head is still undef');
