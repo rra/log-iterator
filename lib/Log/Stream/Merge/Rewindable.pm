5a9e4dd96f67cfb90ec8e370acfc5fac31401e95
Russ Allbery <rra@stanford.edu>
2013-01-23 16:45:23 -0800

    Add Log::Stream::Merge::Rewindable
    A subclass of Log::Stream::Merge that ensures that all of the
    streams are rewindable.

---
+++ lib/Log/Stream/Merge/Rewindable.pm
@@ -0,0 +1,193 @@
+# Log::Stream::Merge::Rewindable -- Merge rewindable log streams
+#
+# Written by Russ Allbery <rra@stanford.edu>
+# Copyright 2013
+#     The Board of Trustees of the Leland Stanford Junior University
+
+##############################################################################
+# Modules and declarations
+##############################################################################
+
+package Log::Stream::Merge::Rewindable;
+
+use 5.010;
+use strict;
+use warnings;
+
+use base qw(Log::Stream::Merge);
+
+# Module version.  Waiting for Perl 5.12 to switch to the new package syntax.
+our $VERSION = '1.00';
+
+##############################################################################
+# Implementation
+##############################################################################
+
+# Create a new Log::Stream::Merge::Rewindable object that ensures all the
+# merged streams are rewindable.
+#
+# $class   - Class of the object being created
+# $code    - Code reference for the merge function
+# @streams - The underlying streams to merge
+#
+# Returns: New Log::Stream::Merge::Rewindable object
+sub new {
+    my ($class, $code, @streams) = @_;
+
+    # Make all the streams rewindable.
+    for my $stream (@streams) {
+        if (!$stream->isa('Log::Stream::Rewindable')) {
+            $stream = Log::Stream::Rewindable->new($stream);
+        }
+    }
+
+    # Defer all other work to our parent class.
+    return $class->SUPER::new($code, @streams);
+}
+
+##############################################################################
+# Module return value and documentation
+##############################################################################
+
+1;
+__END__
+
+=for stopwords
+API Kaufmann MERCHANTABILITY NONINFRINGEMENT STREAMs sublicense
+
+=head1 NAME
+
+Log::Stream::Merge::Rewindable - Merge rewindable log streams
+
+=head1 SYNOPSIS
+
+    use Log::Stream::Merge;
+    my ($stream1, $stream2); # some existing streams
+
+    # Merge function may assume all streams are rewindable.  This one
+    # only returns elements from $stream1 seen in the top 10 items of
+    # $stream2 (possibly out of order).
+    my $code = sub {
+        my ($one, $two) = @_;
+      ONE: {
+            my $element = $one->get;
+            return if !defined $element;
+            $two->bookmark;
+          TWO:
+            for my $i (1 .. 10) {
+                last TWO if !defined $two->head;
+                next TWO if $two->get ne $element;
+
+                # Drop the element we found so we don't match twice.
+                my @saved = $two->saved;
+                pop @saved;
+                $two->discard;
+                $two->prepend(@saved);
+                return $element;
+            }
+            $two->rewind;
+            redo ONE;
+        }
+    };
+
+    # Implement a stream using the above algorithm.
+    my $stream = Log::Stream::Merge->new($code, $stream1, $stream2);
+
+=head1 REQUIREMENTS
+
+Perl 5.10 or later.
+
+=head1 DESCRIPTION
+
+Log::Stream::Merge::Rewindable is identical to L<Log::Stream::Merge>
+except that it ensures that all of its underlying streams are rewindable
+by wrapping them in Log::Stream::Rewindable if necessary.  This allows
+the merge function to use the features of rewindable streams to examine
+the contents of the streams being merged.
+
+A typical use for this type of stream would be code that correlates and
+combines information from two different logs, where the entries in one of
+the logs can be out of order relative to the other.  Rewindable streams
+allows the merge function to look forward in one log for entries matching
+the current entry in the other log and then rewind or prepend unprocessed
+log entries when finished matching the current entry.
+
+=head1 CLASS METHODS
+
+=over 4
+
+=item new(CODE, STREAM, ...)
+
+Create a new merged stream based on the provided STREAMs.
+
+CODE is a reference to a user-provided merge function and will be called
+for each new element and passed, as arguments, all of the STREAM arguments
+to this constructor in the same order as they were passed to the
+constructor, possibly wrapped in Log::Stream::Rewindable if necessary.
+They will continue to be passed even if they've returned end of stream.
+CODE is expected to return the next element in the merged stream each time
+it is called.  Unlike in Log::Stream::Merge, CODE is mandatory.
+
+STREAM is treated as a duck-typed stream, which means that it can be any
+object that supports the get() method with the expected stream semantics.
+(Of course, the user-provided CODE method can make additional assumptions
+about the streams if warranted.)
+
+=back
+
+=head1 INSTANCE METHODS
+
+=over 4
+
+=item get()
+
+Returns the next element in the merged stream and consumes it.  Repeated
+calls to get() will read through the entire merged stream, returning each
+element once.  Returns undef at the end of the stream.
+
+=item head()
+
+Returns the next element in the merged stream without consuming it.
+Repeated calls to head() without an intervening call to get() will keep
+returning the same record.  Returns undef at the end of the stream.
+
+=back
+
+=head1 AUTHOR
+
+Russ Allbery <rra@stanford.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 The Board of Trustees of the Leland Stanford Junior
+University
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+=head1 SEE ALSO
+
+L<Log::Stream>, L<Log::Stream::Merge>, L<Log::Stream::Rewindable>
+
+Dominus, Mark Jason.  I<Higher Order Perl>.  San Francisco: Morgan
+Kaufmann Publishers, 2005.  Print.
+
+The current version of this module is always available from its web site
+at L<http://www.eyrie.org/~eagle/software/log-stream/>.
+
+=cut
---
+++ t/stream/merge/rewindable.t
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+#
+# Test for merging of rewindable streams.
+#
+# Written by Russ Allbery <rra@stanford.edu>
+# Copyright 2013
+#     The Board of Trustees of the Leland Stanford Junior University
+#
+# See LICENSE for licensing terms.
+
+use 5.010;
+use strict;
+use warnings;
+
+use File::Spec;
+use Readonly;
+
+use Test::More tests => 16;
+
+# Load the modules.
+BEGIN {
+    use_ok('Log::Stream');
+    use_ok('Log::Stream::Merge::Rewindable');
+    use_ok('Log::Stream::Rewindable');
+}
+
+# The threshold for how far we'll look into the second stream for elements
+# that match the first stream.
+Readonly my $THRESHOLD => 10;
+
+# Build two trivial little streams.
+my @data_one   = qw(11 4 17 2 4 11);
+my $code_one   = sub { return shift @data_one };
+my $stream_one = Log::Stream->new({ code => $code_one });
+isa_ok($stream_one, 'Log::Stream');
+my @data_two   = qw(1 2 3 4 5 6 7 8 9 10 11);
+my $code_two   = sub { return shift @data_two };
+my $stream_two = Log::Stream->new({ code => $code_two });
+isa_ok($stream_two, 'Log::Stream');
+
+# Make the first stream rewindable so that we can test both branches.
+$stream_one = Log::Stream::Rewindable->new($stream_one);
+isa_ok($stream_one, 'Log::Stream::Rewindable');
+
+# Our merging algorithm is the example from the module synopsis.  We return
+# only those elements from the first stream where a matching element occurs
+# in the second stream within the top ten elements.
+my $code = sub {
+    my ($one, $two) = @_;
+  ONE: {
+        my $element = $one->get;
+        return if !defined $element;
+        $two->bookmark;
+      TWO:
+        for my $i (1 .. $THRESHOLD) {
+            last TWO if !defined $two->head;
+            next TWO if $two->get ne $element;
+
+            # Drop the element we found so we don't match twice.
+            my @saved = $two->saved;
+            pop @saved;
+            $two->discard;
+            $two->prepend(@saved);
+            return $element;
+        }
+        $two->rewind;
+        redo ONE;
+    }
+};
+
+# Build a merged stream with that algorithm.
+my $stream = eval {
+    Log::Stream::Merge::Rewindable->new($code, $stream_one, $stream_two);
+};
+is($@, q{}, 'No exceptions on merge object creation');
+if ($stream) {
+    isa_ok($stream, 'Log::Stream::Merge::Rewindable');
+} else {
+    ok(0, 'Object creation failed');
+    BAIL_OUT('cannot continue without Log::Stream::Merge::Rewindable object');
+}
+
+# Check the output.
+for my $data (qw(4 2 11)) {
+    is($stream->head, $data, "head() returns $data");
+    is($stream->get,  $data, "get() returns $data");
+}
+is($stream->head, undef, 'head() is undef at end of stream');
+is($stream->get,  undef, 'get() is undef at end of stream');
